419b79d0261d36db9fe1898d786b46e349fb294a
Courtney Noonan <courtneynoonan@me.com>
2017-03-03 09:48:04 -0500

    building photos component

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import App from './app.jsx';
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './app.jsx'
 
-render( <AppContainer><App/></AppContainer>, document.querySelector("#app"));
+render( <AppContainer><App/></AppContainer>, document.querySelector("#app"))
 
 if (module && module.hot) {
-  module.hot.accept('./app.jsx', () => {
+  module.hot.accept('./game.jsx', () => {
     render(
       <AppContainer>
         <App/>
       </AppContainer>,
       document.querySelector("#app")
-    );
-  });
+    )
+  })
 }
---
+++ src/photos.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import '../styles/main.scss'
+import axios from 'axios'
+
+function Photo(props) {
+  return (
+    <div>
+      <h2>{props.photo.description} ({props.photo.date_created})</h2>
+      <img className="photo" src={props.photo.picture} />
+    </div>
+    )
+}
+
+export default class Photos extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      photos: [],
+    }
+  }
+
+  componentDidMount() {
+    const url = (this.props.category)
+      ? `http://localhost:5000/photos/${this.props.category}/JSON`
+      : 'http://localhost:5000/photos/JSON'
+    this.serverRequest =
+      axios.get(url).then((result) => {
+        console.log(result)
+        this.setState(Object.assign({}, this.state, {photos: result.data.photos}))
+      })
+  }
+
+  render() {
+    return (
+      <div className="photos">
+        {this.state.photos.map(photo => <Photo key={photo.id} photo={photo} />)}
+      </div>
+    )
+  }
+
+}
