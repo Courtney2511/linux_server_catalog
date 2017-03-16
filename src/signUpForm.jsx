8e5dddee7d8cb02f8e7a9373169da61254a6bbf4
Courtney Noonan <courtneynoonan@me.com>
2017-03-16 13:35:46 -0400

    displaying server side errors for signup form

--- src/signUpForm.jsx
+++ src/signUpForm.jsx
@@ -9,7 +9,10 @@ export default class SignUpForm extends React.Component {
     this.state = {
       username: '',
       email: '',
-      password: ''
+      password: '',
+      error_username: '',
+      error_email: '',
+      error_password: ''
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -26,12 +29,21 @@ export default class SignUpForm extends React.Component {
   }
 
   handleSubmit(event) {
+    const self = this
     event.preventDefault()
     axios.post('http://localhost:5000/signup', {
       username: this.state.username,
       email: this.state.email,
       password: this.state.password
     }).then(function(response) {
+      const data = response.data
+      if (!data.success) {
+        self.setState({
+          error_username: data.error_username,
+          error_email: data.error_email,
+          error_password: data.error_password
+        })
+      }
       console.log(response)
     }).catch(function(error) {
       console.log(error)
@@ -43,9 +55,9 @@ export default class SignUpForm extends React.Component {
       <div className = "form-container">
         <h2>Join our community!</h2>
         <form action="" method="POST" onSubmit={ this.handleSubmit }>
-	         <input type="text" name="username" value={ this.state.username } onChange={ this.handleChange } placeholder="name"/>
-           <input type="text" name="email" value={ this.state.email } onChange={ this.handleChange } placeholder="email"/>
-	         <input type="text" name="password" value={ this.state.password } onChange= { this.handleChange } placeholder="password"/>
+	         <input type="text" name="username" value={ this.state.username } onChange={ this.handleChange } placeholder="name"/> { this.state.error_username }
+           <input type="text" name="email" value={ this.state.email } onChange={ this.handleChange } placeholder="email"/> { this.state.error_email}
+	         <input type="text" name="password" value={ this.state.password } onChange= { this.handleChange } placeholder="password"/> { this.state.error_password}
           <div className="form-submit">
             <input className="submit-button" type="submit" value="Let's Go!"></input>
           </div>
