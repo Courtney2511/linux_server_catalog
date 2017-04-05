374c2b66faa2a90aa4b44c05950ee6002a89b6fd
Courtney Noonan <courtneynoonan@me.com>
2017-04-05 15:04:00 -0400

    adding new post link to dashboard

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router'
+
 
 export default function Dashboard(props) {
 
   return(
     <div>
       <div className="welcome">
-        <p>Hello {props.user.username} :)</p>
+        <p className="hello">Hello {props.user.username} :)</p>
+      </div>
+      <div className="add-post">
+        <i className="fa fa-camera" aria-hidden="true"></i>
+        <Link to="/photos/new" className="post">Post</Link>
       </div>
       <div className="logout">
         <i className="fa fa-sign-out" aria-hidden="true"></i>
