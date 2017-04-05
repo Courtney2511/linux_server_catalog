29ee617a3680a138529c03e5283219e269196bc1
Courtney Noonan <courtneynoonan@me.com>
2017-04-05 14:37:00 -0400

    styling dashboard to match login panel

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -4,8 +4,13 @@ export default function Dashboard(props) {
 
   return(
     <div>
-      <h3>Welcome {props.user.username}</h3>
-      <button id="logout" onClick={() => props.actions.logOutUser(props.user.jwtToken)}>Log Out</button>
+      <div className="welcome">
+        <p>Hello {props.user.username} :)</p>
+      </div>
+      <div className="logout">
+        <i className="fa fa-sign-out" aria-hidden="true"></i>
+        <button id="logout" onClick={() => props.actions.logOutUser(props.user.jwtToken)}>Log Out</button>
+      </div>
     </div>
   )
 }
