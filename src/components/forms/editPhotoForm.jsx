d9cda697e3c18b987bfa015bbf9f6ea9ef90b297
Courtney Noonan <courtneynoonan@me.com>
2017-05-01 15:26:15 -0400

    refactoring category values to use category id

--- src/components/forms/editPhotoForm.jsx
+++ src/components/forms/editPhotoForm.jsx
@@ -6,17 +6,17 @@ class EditPhotoForm extends React.Component {
   render() {
     const { handleSubmit } = this.props
     return (
-      <LocalForm model="photo" onSubmit={(values) => handleSubmit(values)} initialState={this.props.photo}>
+        <LocalForm model="photo" onSubmit={(values) => handleSubmit(values)} initialState={this.props.photo}>
         <Control.text model=".picture" />
         <Control.text model=".name" />
         <Control.textarea model=".description"></Control.textarea>
         <div>
           <Control.select model=".category">
-            <option value="Animals">Animals</option>
-            <option value="Black & White">Black & White</option>
-            <option value="Landscape">Landscape</option>
-            <option value="People">People</option>
-            <option value="Food">Food</option>
+            <option value="1">Animals</option>
+            <option value="2">Black & White</option>
+            <option value="3">Landscape</option>
+            <option value="4">People</option>
+            <option value="5">Food</option>
           </Control.select>
         </div>
         <div>
